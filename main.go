6af7765537f40944696a6a8f625eadb35710832b
Andrey <andreykononenko@lodoss.com>
2018-07-02 16:36:47 +0300

    init

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "log"
+  "github.com/gin-gonic/gin"
+  "github.com/AdminLP/go-todo/db"
+  TaskController "github.com/AdminLP/go-todo/controllers"
+)
+
+func main() {
+  log.Println("Starting server..")
+
+  db.Init();
+
+  r := gin.Default()
+
+  v1 := r.Group("/api/v1")
+  {
+    tasks := v1.Group("/tasks")
+    {
+      tasks.GET("/", TaskController.GetTasks)
+      tasks.POST("/", TaskController.CreateTask)
+      tasks.PUT("/:id", TaskController.UpdateTask)
+      tasks.DELETE("/:id", TaskController.DeleteTask)
+    }
+  }
+
+  r.Run()
+}
